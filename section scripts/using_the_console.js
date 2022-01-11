331fb4ea5167b8be4b1edb17485f62f06b9694f3
Abbaas Mhlongo <jamie.web.dev.pro@gmail.com>
2022-01-11 11:48:54 +0200

    first commit

---
+++ section scripts/using_the_console.js
@@ -0,0 +1,47 @@
+// Single Line Comments
+
+/*
+    Multi Line Comments 
+*/
+
+// Logging Text onto the console
+console.log('Hello World');
+
+// Logging Numbers onto the console
+console.log(123);
+
+// Logging an Object onto the console
+console.log({ Name: 'Abbaas', Age: 12 })
+
+// Logging an Array onto the console
+console.log(['Sarah', 'John', 'Carter'])
+
+// Logging Booleans onto to the console
+console.log(true, false)
+
+// Logging type of undefined onto the console
+console.log(undefined)
+
+// Logging type of Null onto the console
+console.log(null)
+
+// Logging type of NaN
+console.log(NaN)
+
+// Logging an object in the form of a table by using console.table()
+console.table({ a: 1, b: 2, c: 3 })
+
+// TYPES OF MESSAGES WE CAN LOG
+
+// Error Messages
+console.error('There is an error in your code');
+
+// Warning Message
+console.warn('Be careful not to accept cookies from strangers')
+
+// Time your code
+console.time()
+
+console.table({ a: 1, b: 2, c: 3 })
+
+console.timeEnd()
