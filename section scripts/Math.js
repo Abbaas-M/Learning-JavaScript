1b8a8f9dc175469a5c936fb36c8a14fccba0adb8
Abbaas Mhlongo <89652142+Abbaas-M@users.noreply.github.com>
2022-10-18 06:42:10 +0200

    Math Object
    This is some practice code displaying some comman and useful methods and properties of the Math object

---
+++ section scripts/Math.js
@@ -0,0 +1,45 @@
+// Simple math operations
+let num1 = 50;
+let num2 = 100;
+let val;
+
+// val = num1 + num2;
+// val = num1 * num2;
+// val = num1 / num2;
+// val = num1 - num2;
+// val = num1 % num2;
+
+// MATH OBJECT
+
+// PI
+val = Math.PI;
+
+// Rounding Off
+val = Math.round(34.4);
+
+// Rounding Up
+val = Math.ceil(24.8)
+
+// Rounding down
+val = Math.floor(23.8);
+
+// Sqaure root of a number
+val = Math.sqrt(64);
+
+// Cubing Numbers
+val = Math.cbrt(64);
+
+// Powers Of Numbers
+val = Math.pow(2, 2);
+
+// Min
+val = Math.min(43, 23, 42, 12);
+
+// Max
+val = Math.max(2, 34, 23, 56, 778, 5, 345, 353, 63, 53523, 5634, 356);
+
+// Random
+val = parseInt(Math.random() * 100 + 1);
+
+
+console.log(val)
