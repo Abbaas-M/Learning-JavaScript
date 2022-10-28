fd2addd3eb1ebf29c4f7a9e701e8744dfd16d1a0
Abbaas Mhlongo <89652142+Abbaas-M@users.noreply.github.com>
2022-10-28 15:04:22 +0200

    Create switch_statements.js

---
+++ section scripts/switch_statements.js
@@ -0,0 +1,30 @@
+// Switch Statements
+
+// let gender = 'female';
+
+// switch (gender) {
+//     case 'male':
+//         console.log('Go to male toilets')
+//         break;
+//     case 'female':
+//         console.log('Go to female toilets');
+//         break;
+
+// }
+
+// let hairColor = 'Black';
+
+// switch (hairColor) {
+//     case 'Yellow':
+//         console.log('Blonde');
+//         break;
+//     case 'Orange':
+//         console.log('Kitty');
+//         break;
+//     case 'Black':
+//         console.log('Normal Color');
+//         break;
+//     default:
+//         console.log('Not Known')
+// }
+
