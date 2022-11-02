bd6478ffe6cf4a82ed4564e58966011bf092f5a3
Abbaas Mhlongo <89652142+Abbaas-M@users.noreply.github.com>
2022-11-02 04:49:47 +0200

    Create window_object.js

---
+++ section scripts/window_object.js
@@ -0,0 +1,46 @@
+// WINDOW OBJECT
+// OBJECTS/PROPERTIES/METHODS
+
+// PROMT
+// let input = prompt('Hello user, how are you?');
+
+// Alert
+// alert(123);
+
+// CONFIRM
+// if (confirm(`Are you sure you would like to delete this item?`)) {
+//     alert('Item Deleted')
+// } else {
+//     alert(`Deletion of item cancelled`);
+// }
+
+// INNERHEIGHT / INNERWIDTH
+// console.log(window.innerHeight);
+// console.log(window.innerWidth);
+
+// if (window.innerWidth <= 700) {
+//     document.body.style.background = 'steelblue';
+//     document.body.style.color = '#fff';
+// } else {
+//     document.body.style.background = 'rebeccapurple';
+//     document.body.style.color = 'plum';
+// }
+
+// OUTERWIDTH / OUTER HEIGHT
+// console.log(window.outerHeight);
+// console.log(window.outerWidth);
+
+// SCROLL POINTS
+// console.log(window.scrollY);
+// console.log(window.scrollX);
+
+// LOCATION OBJECT
+// console.log(location);
+// window.location.href = 'https://worldofdevs.com';
+
+// HISTORY OBJECT
+// console.log(window.history.go(-2))
+// console.log(window.history.length);
+
+// NAVIGATOR
+// window.navigator;
