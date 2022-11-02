e014693ece2a40cc313a31e359e2659e93e9fdd4
Abbaas Mhlongo <89652142+Abbaas-M@users.noreply.github.com>
2022-11-02 09:39:24 +0200

    Create exploring_the_dom.js

---
+++ exploring_the_dom.js
@@ -0,0 +1,61 @@
+// EXPLORING THE DOM
+let val;
+
+// let fruits = ['Bannana', 'Apple', 'Pear', 'Orange', 'Raspberry'];
+
+// fruits.forEach(function (fruit) {
+//     document.body.innerHTML += `
+//     <li class="list-item fruit" id="list-item">${fruit}</li>
+
+//     `
+// })
+// Gets all the elements on the page in a node list
+// val = document.all;
+
+// Get specific elements from the DOM
+// val = document.body;
+// val = document.doctype;
+// val = document.head;
+// val = document.domain;
+// val = document.URL;
+// val = document.characterSet;
+// val = document.contentType;
+
+// Getting classname
+// val = document.all[11];
+// val = document.all[11].className += ' fruit-list-item';
+// val = document.all[12].className = 'fruit-list-item';
+// val = document.all[13].className = 'fruit-list-item';
+// val = document.all[14].className = 'fruit-list-item';
+// val = document.all[15].className = 'fruit-list-item';
+
+// Getting Attribute
+// val = document.all[8].getAttribute('style');
+
+// let imgs = document.images;
+// let imgArr = Array.from(imgs);
+// imgArr.forEach(function (img) {
+//     console.log(img.getAttribute('src'));
+// })
+
+// Selector properties
+// val = document.forms;
+// val = document.images;
+// val = document.links;
+// val = document.scripts[0];
+
+// val = document.images[3].getAttribute('alt');
+
+// CONVERTING AN HTML COLLECTION TO AN ARRAY
+// let imgs = document.images;
+// let imgArr = Array.from(imgs);
+
+// imgArr.forEach(function (img) {
+//     console.log(img.getAttribute('alt'));
+//     console.log(img.getAttribute('src'));
+// })
+
+
+
+
+console.log(val)
