824e161c77ad2fb17e855d8c430f84f03de4a801
Abbaas Mhlongo <89652142+Abbaas-M@users.noreply.github.com>
2022-11-09 16:15:15 +0200

    Create multiple_element_selector.js

---
+++ multiple_element_selector.js
@@ -0,0 +1,27 @@
+// SELECTING MULTIPLE ELEMENTS BY THEIR CLASS
+// let tasks = document.getElementsByClassName('task');
+
+// SELECTING ELEMENTS WITHIN A SPECIFIC TAG
+// let tasks = document.querySelector('.task-items').getElementsByClassName('task');
+
+// GETTING ELEMENT BY ITS TAG NAME
+// let tasks = document.getElementsByTagName('li');
+
+// LOOPING THROUGH AN ARRAY USING FOR LOOP
+// for (let i = 0; i < tasks.length; i++) {
+//     tasks[i].style.background = 'seagreen';
+//     tasks[i].style.color = '#fff';
+// }
+
+// QUERYSELECTORALL()
+// let tasks = document.querySelectorAll('.task:nth-of-type(odd)');
+// let tasksEv = document.querySelectorAll('.task:nth-of-type(even)');
+
+// tasks.forEach(function (task) {
+//     task.style.background = 'lightGray'
+// })
+
+// for (let i = 0; i < tasksEv.length; i++) {
+//     tasksEv[i].style.background = 'darkgray';
+// }
+
